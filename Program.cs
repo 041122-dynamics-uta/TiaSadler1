a931fb81a114dd8df2fca58e96e7783d3f3a4876
Tia Sadler <thenewsadler@outlook.com>
2022-04-21 10:02:39 -0400

    This is my switch case app project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace switchCaseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you enjoy coding ? (yes/no/maybe)");
+            string input = Console.ReadLine();
+
+            switch(input?.ToLower())
+
+                {
+                    case "yes":
+                    case "maybe":
+                    Console.WriteLine("Awesome!");
+                    break;
+                    case "no":
+                    Console.WriteLine("Learn to like it!");
+                    break;
+                    default:
+                    Console.WriteLine("I'm sorry, I don't understand what you wrote!");
+                    break;
+                }
+
+        }
+    }
+}
